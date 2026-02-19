ea62f8807e2e6c73d160f6f0219dcfb8bc89e3b2
dk-xy <92148260+dk-xy@users.noreply.github.com>
2026-02-19 13:54:48 +0100

    structure and fonts

--- wp-content/themes/boat-school/front-page.php
+++ wp-content/themes/boat-school/front-page.php
@@ -2,8 +2,9 @@
 
 <main id="primary" class="site-main">
 
-    <section class="my-hero-section">
 
+    <!-- HERO SECTION----------------------------------------------------------------------------------------- -->
+    <section class="my-hero-section website-section">
         <?php
         // 1. Fetch the image URL from ACF
         $hero_url = get_field('homepage_hero_image');
@@ -19,7 +20,7 @@
                 <img src="<?php echo get_template_directory_uri(); ?>/assets/logo.png" alt="" class="logo-hero">
             </div>
             <div class="image-container">
-                <img src="<?php echo esc_url($hero_url); ?>" alt="Dynamic Homepage Hero" class="js-parallax-target">
+                <img src="<?php echo esc_url($hero_url); ?>" alt="Dynamic Homepage Hero" class="hero-intro-picture">
             </div>
 
         </div>
@@ -28,15 +29,153 @@
                 ?> -->
         <div class="hero-intro">
             <div class="hero-title">
-                <h1>Cours de bâteau</h1>
-                <div>à Grandson, sur le lac de Neuchâtel</div>
+                <h1>Bâteau École Grandson</h1>
+                <h2 class="highlight-title">Cours de bâteau, sur le lac de Neuchâtel</h2>
+                <!-- <div class="sub-text">Cours de bâteau, sur le lac de Neuchâtel</div> -->
             </div>
 
             <div class="hero-descr">
-                Bienvenue au Bateau-école de Grandson. Vous souhaitez obtenir votre permis de navigation moteur (catégorie A) sur le lac de Neuchâtel ? Nous vous proposons une formation complète, alliant théorie et pratique, pour naviguer en toute sérénité.
+               Vous souhaitez obtenir votre permis de navigation moteur (catégorie A) sur le lac de Neuchâtel ? </br> Nous vous proposons une formation complète, alliant théorie et pratique, pour naviguer en toute sérénité.
+            </div>
+        </div>
+    </section>
+    <!-- SERVICES SECTION----------------------------------------------------------------------------------------- -->
+
+    <section  class="website-section">
+        <div class="services-section-title-block">
+            <div class="section-heading">
+                <div class="title-header">
+                    Nos services
+                </div>
+                <h2>
+                    Formation au permis de conduire moteur (Cat. A)
+                </h2>
             </div>
 
+            <div>
+                Notre école de navigation vous prépare à l’examen officiel de conduite. Idéalement situé près d’Yverdon-les-Bains, le port de Grandson offre un accès direct aux plus belles zones du lac de Neuchâtel.
+            </div>
+        </div>
+
+        <div class="services-blocks-container">
+
+            <div class="service-block">
+                <div class="service-block-title">Le bateau</div>
+                <div class="service-block-descr">
+                    Confortable et maniable, notre bateau est le partenaire idéal pour vos premières manœuvres comme pour vos examens de conduite.
+                </div>
+            </div>
+
+            <div class="service-block">
+                <div class="service-block-title">Les cours</div>
+                <div class="service-block-descr">
+                    Nos leçons sont flexibles, adaptées à votre rythme et axées sur la sécurité et la maîtrise technique.
+                </div>
+            </div>
+
+            <div class="service-block">
+                <div class="service-block-title">Le lieu</div>
+                <div class="service-block-descr">
+                    Entre eaux calmes et infrastructures portuaires de qualité, vous apprenez à naviguer dans l’un des plus beaux environnements de la région.
+                </div>
+            </div>
+
+        </div>
+
     </section>
+
+
+
+    <!-- BENEFIT SECTION----------------------------------------------------------------------------------------- -->
+    <section  class="website-section">
+        <div class="benefit-intro-section">
+
+            <div class="section-heading">
+                <div class="title-header">
+                    Les cours sur mesure
+                </div>
+                <h2>
+                    Votre réussite au fil de l’eau
+                </h2>
+            </div>
+
+            <div class="benefit-intro-descr">
+                Obtenir son permis de navigation moteur (catégorie A) est une étape passionnante. Notre méthode d’enseignement repose sur une approche progressive et personnalisée, conçue pour vous mettre en confiance dès la première sortie sur le lac de Neuchâtel
+            </div>
+
+        </div>
+
+        <div class="benefit-blocks-container">
+
+            <div class="benefit-block">
+                <div class="benefit-block-title">Formation complète</div>
+                <div class="benefit-block-descr">
+                    De la théorie à la pratique, nous couvrons tous les aspects nécessaires pour réussir votre examen de conduite.
+                </div>
+            </div>
+
+            <div class="benefit-block">
+                <div class="benefit-block-title">Flexibilité</div>
+                <div class="benefit-block-descr">
+                    Nos horaires s’adaptent à votre emploi du temps, avec des sessions disponibles en semaine et le week-end.
+                </div>
+            </div>
+
+            <div class="benefit-block">
+                <div class="benefit-block-title">Environnement idéal</div>
+                <div class="benefit-block-descr">
+                    Apprenez à naviguer dans un cadre naturel exceptionnel, avec des conditions de navigation variées pour une expérience d’apprentissage riche.
+                </div>
+            </div>
+
+        </div>
+
+    </section>
+
+    <!-- TEACHER SECTION----------------------------------------------------------------------------------------- -->
+    <section  class="website-section">
+        <div class="section-heading">
+            <div class="title-header">
+                Raphaël Party, Votre capitaine
+            </div>
+            <h2>
+                L’expérience au service de votre réussite
+            </h2>
+        </div>
+
+        <div class="teacher-section-container">
+            <div class="teacher-image-container">
+                <img src="<?php echo get_template_directory_uri(); ?>/assets/images/teacher.jpg" alt="Votre capitaine" class="teacher-image">
+            </div>
+            <div class="teacher-descr">
+                Naviguer est une passion, mais transmettre cet art est une vocation. Avec une parfaite connaissance du lac de Neuchâtel et des exigences de l’examen de navigation, je vous accompagne avec calme et méthode pour faire de vous un navigateur chevronné.
+            </div>
+        </div>
+    </section>
+
+    <!-- TESTIMONIALS SECTION----------------------------------------------------------------------------------------- -->
+    <section  class="website-section">
+
+    </section>
+
+    <!-- GALLERIES SECTION----------------------------------------------------------------------------------------- -->
+    <section  class="website-section">
+
+    </section>
+
+    <!-- CONTACT SECTION----------------------------------------------------------------------------------------- -->
+    <section class="website-section">
+        <div class="section-heading">
+            <div class="title-header">
+                Une question ?
+            </div>
+            <h2>
+                Contactez-nous !
+            </h2>
+        </div>
+    </section>
+
+
 </main>
 
 <?php get_footer(); ?>
